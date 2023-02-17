e394a198c9a4463e5e03e05653007c3ef7ea7bf3
milan-ljmu <m.odedra@2021.ljmu.ac.uk>
2023-02-17 13:06:52 +0000

    added plot points and countries and recovered data

--- recoveries-and-deaths/country-recovery-data.py
+++ recoveries-and-deaths/country-recovery-data.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-confirmed = pd.read_csv('Dataset\Covid-19 Dataset\country_wise_latest.csv')
+confirmed_cases = pd.read_csv('Dataset\Covid-19 Dataset\country_wise_latest.csv')
 # countries_recovered = pd.read_csv('country_wise_latest.csv')
 # deaths = pd.read_csv('country_wise_latest.csv')
 
-print(confirmed)
+# print(confirmed_cases)
+confirmed = confirmed.drop(['Country/Region', 'recovered'])
+
+# confirmed.plot()
+# plt.show()
