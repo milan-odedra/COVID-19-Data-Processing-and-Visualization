5285c2f482173b4ed998cdd16f1fea8da6b70436
milan-ljmu <m.odedra@2021.ljmu.ac.uk>
2023-02-17 12:44:23 +0000

    added read csv line

--- recoveries-and-deaths/country-recovery-data.py
+++ recoveries-and-deaths/country-recovery-data.py
@@ -1,2 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
+confirmed = pd.read_csv('Dataset\Covid-19 Dataset\country_wise_latest.csv')
+# countries_recovered = pd.read_csv('country_wise_latest.csv')
+# deaths = pd.read_csv('country_wise_latest.csv')
+
+print(confirmed)
