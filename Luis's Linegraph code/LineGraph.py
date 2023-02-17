fdec31720ee112a3804790257875f55be013f84c
Luis Pullan <l.pullan@2021.ljmu.ac.uk>
2023-02-17 11:31:10 +0000

    Added comments

--- Luis's Linegraph code/LineGraph.py
+++ Luis's Linegraph code/LineGraph.py
@@ -1,5 +1,9 @@
+#Remember to download the "pandas" library before running code.
+#Use "pip install pandas" in the cmd before hand.
 import pandas as pd
-
+#This puts the data from day_wise file into the dayVals variable
 dayVals=pd.read_csv("Dataset\Covid-19 Dataset\day_wise.csv")
 
 print(dayVals.to_string())
+"""for x in dayVals:
+    print (x)"""
