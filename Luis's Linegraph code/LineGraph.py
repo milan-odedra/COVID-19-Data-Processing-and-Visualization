8e4a3b445552bccef64aa37b0c6a18e341dbadf8
Luis Pullan <l.pullan@2021.ljmu.ac.uk>
2023-02-17 13:43:43 +0000

    Trying to split the list between Uk and the world

--- Luis's Linegraph code/LineGraph.py
+++ Luis's Linegraph code/LineGraph.py
@@ -1,9 +1,21 @@
 #Remember to download the "pandas" library before running code.
-#Use "pip install pandas" in the cmd before hand.
+#Use "pip install pandas" in the cmd before hand. Do the same for matplotlib.pyplot
 import pandas as pd
-#This puts the data from day_wise file into the dayVals variable
-dayVals=pd.read_csv("Dataset\Covid-19 Dataset\day_wise.csv")
+import matplotlib.pyplot as plt
+# This puts the data from day_wise file into the dayVals variable
+# dayVals=pd.read_csv("Dataset\Covid-19 Dataset\day_wise.csv")
+UK=[]
+with open ("Dataset\Covid-19 Dataset\\full_grouped.csv","r") as split:
+    line = split.readlines()
+    for x in line:
+        if x=="United Kingdom":
+            UK.append(x)
+print(UK)            
+# dayVals=pd.read_csv("Dataset\Covid-19 Dataset\\full_grouped.csv")
+# #print(dayVals.to_string())
+# print(dayVals)
+# # for x in dayVals:
+# #     print (x)
 
-print(dayVals.to_string())
-"""for x in dayVals:
-    print (x)"""
+# dayVals.plot()
+# plt.show()
