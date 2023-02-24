5646b3fc9268550ba00a75e2ebe2ce8bd301c5c3
Luis Pullan <l.pullan@2021.ljmu.ac.uk>
2023-02-24 11:07:54 +0000

    Separated UK from the csv file

--- Luis's Linegraph code/LineGraph.py
+++ Luis's Linegraph code/LineGraph.py
@@ -1,26 +1,16 @@
 #Remember to download the "pandas" library before running code.
-#Use "pip install pandas" in the cmd before hand. Do the same for matplotlib.pyplot
+#Use "pip install pandas" in the cmd before hand. Do the same for matplotlib
 import pandas as pd
 import matplotlib.pyplot as plt
 # This puts the data from day_wise file into the dayVals variable
-# dayVals=pd.read_csv("Dataset\Covid-19 Dataset\day_wise.csv")
-UK=[]
-# with open ("Dataset\Covid-19 Dataset\\full_grouped.csv","r") as split:
-#     line = split.readlines()
-#     print(line)
-#     for x in line:
-#         if x=="United Kingdom":
-#             UK.append(x)
-      
 dayVals=pd.read_csv("Dataset\Covid-19 Dataset\\full_grouped.csv")
-for x in (dayVals):
-    for y in x:
-        if x==("United Kingdom"):
-            UK.append(x)
+UK=dayVals[(dayVals["Country/Region"]==("United Kingdom"))]
+print (UK)
+
 
 #print(dayVals.to_string())
 # print(dayVals)
 
-# dayVals.plot()
-# plt.show()
+dayVals.plot()
+plt.show()
 print(UK)
