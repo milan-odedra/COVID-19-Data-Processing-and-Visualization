15fa960b5a2ba6abdfff372e0e2e332cbafe7e23
milan-ljmu <m.odedra@2021.ljmu.ac.uk>
2023-02-24 12:07:01 +0000

    started to read files

---
+++ covid19_analysis.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib as plt
+
+confirmed = pd.read_csv('covid19_confirmed.csv')
+deaths = pd.read_csv('covid19_deaths.csv')
+recoveries = pd.read_csv('covid19_reocoveries.csv')
+
