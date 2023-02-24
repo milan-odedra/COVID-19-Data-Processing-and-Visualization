47da55fe28a2fb2dcc7f2e4f836f83883fac0b19
milan-ljmu <m.odedra@2021.ljmu.ac.uk>
2023-02-24 12:14:56 +0000

    started grouping the data of countries

--- covid19_analysis.py
+++ covid19_analysis.py
@@ -3,5 +3,13 @@ import matplotlib as plt
 
 confirmed = pd.read_csv('covid19_confirmed.csv')
 deaths = pd.read_csv('covid19_deaths.csv')
-recoveries = pd.read_csv('covid19_reocoveries.csv')
+recoveries = pd.read_csv('covid19_recoveries.csv')
+
+confirmed = confirmed.drop(['Province/State', 'Lat', 'Long'], axis=1)
+
+confirmed = confirmed.groupby(confirmed['Country/Region']).aggregate('sum')
+deaths = confirmed.groupby(deaths['Country/Region']).aggregate('sum')
+recoveries = confirmed.groupby(recoveries['Country/Region']).aggregate('sum')
+
+print(confirmed)
 
