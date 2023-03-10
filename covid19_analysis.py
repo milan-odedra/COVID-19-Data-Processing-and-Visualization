fa0ba8ae744e54146854714d6124c7aa27bd0969
milan-ljmu <m.odedra@2021.ljmu.ac.uk>
2023-03-10 12:50:22 +0000

    Calculation: active cases compared to growth rate

--- covid19_analysis.py
+++ covid19_analysis.py
@@ -1,3 +1,5 @@
+# Live Feed Covid Data Analysis
+
 # Import Libraries
 import pandas as pd
 import matplotlib as plt
@@ -22,25 +24,37 @@ confirmed = confirmed.T
 deaths = deaths.T
 recoveries = recoveries.T 
 
-# create copy to manipulate data
+# New Cases copy
 new_cases = confirmed.copy()
 
 # A day subtracted by previous day giving difference in new cases
 for day in range(1,  len(confirmed)):
     new_cases.iloc[day] = confirmed.iloc[day] - confirmed.iloc[day - 1]
 
-# Prints the cases in the space of 10 days
+# Prints the new cases in the space of 10 days
 #print(new_cases.tail(10))
 #print(confirmed.tail(10))
 
+# Growth rate
 growth_rate = confirmed.copy()
 
 # new cases divided by previous day cases showing percentage increase/decrease from day before
 for day in range(1, len(confirmed)):
     growth_rate.iloc[day] = (new_cases.iloc[day] / confirmed.iloc[day - 1]) * 100
 
-print(growth_rate.tail(10))
+#print(growth_rate.tail(10))
 
-# print(deaths)
-# print(recoveries)
+# Active Cases
+active_cases = confirmed.copy()
+
+for day in range(0, len(confirmed)):
+    active_cases.iloc[day] = confirmed.iloc[day] - deaths.iloc[day] - recoveries.iloc[day]
+
+
+overall_growth_rate = confirmed.copy()
+
+for day in range(1, len(confirmed)):
+    overall_growth_rate.iloc[day] = ((active_cases.iloc[day] - active_cases.iloc[day - 1]) / active_cases.iloc[day-1]) * 100
+
+print(overall_growth_rate.tail(10))
 
