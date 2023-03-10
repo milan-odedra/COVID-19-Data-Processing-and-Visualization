39b673e3b338429b82592754608ba3f50e16bdb1
milan-ljmu <m.odedra@2021.ljmu.ac.uk>
2023-03-10 11:41:48 +0000

    added comments

--- covid19_analysis.py
+++ covid19_analysis.py
@@ -1,22 +1,30 @@
+# Import Libraries
 import pandas as pd
 import matplotlib as plt
 
+# Read data files
 confirmed = pd.read_csv('covid19_confirmed.csv')
 deaths = pd.read_csv('covid19_deaths.csv')
 recoveries = pd.read_csv('covid19_recoveries.csv')
 
+# Shows data for specfic columns 
 confirmed = confirmed.drop(['Province/State', 'Lat', 'Long'], axis=1)
 deaths = deaths.drop(['Province/State', 'Lat', 'Long'], axis=1)
 recoveries = recoveries.drop(['Province/State', 'Lat', 'Long'], axis=1)
 
+# Group the data by countries/regions and sum the data
 confirmed = confirmed.groupby(confirmed['Country/Region']).aggregate('sum')
 deaths = deaths.groupby(deaths['Country/Region']).aggregate('sum')
 recoveries = recoveries.groupby(recoveries['Country/Region']).aggregate('sum')
 
+# returns transposed data 
 confirmed = confirmed.T
 deaths = deaths.T
 recoveries = recoveries.T 
 
-print(deaths)
-print(recoveries)
+
+
+
+# print(deaths)
+# print(recoveries)
 
