26e8105df6bd202ad08bc575b081492764f4012e
Luis Pullan <l.pullan@2021.ljmu.ac.uk>
2023-03-24 11:37:50 +0000

    Turned plot and the region lists mean into func

--- Luis's Linegraph code/LineGraph.py
+++ Luis's Linegraph code/LineGraph.py
@@ -30,7 +30,7 @@ def regionList(List):
         if highestDate!=x:
             highestDate=x
             #Puts the data with the corresponding date to CurrDate
-            CurrDate=dayVals[(List["Date"]==(x))]
+            CurrDate=dayVals[(dayVals["Date"]==(x))]
             #Finds the mean of each column for that date
             Confirmed.append((CurrDate["Confirmed"]).mean())
             Deaths.append((CurrDate["Deaths"]).mean())
@@ -41,28 +41,30 @@ def regionList(List):
             # newRecovered.append((CurrDate["New recovered"]).mean())
             # Timelist.append([Confirmed,Deaths,Recovered,Active,newCases,newDeaths,newRecovered])
     return Confirmed, Deaths, Recovered, Active
-# def plotGraph(Xaxis):
-#     """Plots the graph using the inputs and then shows them"""
-#     fig, ax=plt.subplots(2,figsize=(10,5))
-#     fig.suptitle("Covid-19 cases")
-#     ax[1].set_title("Covid-19 cases in the world")
-#     ax[1].set_xlabel("Dates")
-#     ax[1].set_ylabel("Number of people")
-#     ax[1].plot(Seconds,plot["Confirmed"],label="Confirmed")
-#     ax[1].plot(Seconds,plot["Deaths"],label="Deaths")
-#     ax[1].plot(Seconds,plot["Recovered"],label="Recovered")
-#     ###Incase I want to re-add all of the rest of the plots.###
-#     # ax[1].plot(Seconds,Active,label="Active")
-#     # ax[1].plot(Seconds,newCases,label="New Cases")
-#     # ax[1].plot(Seconds,newDeaths,label="New Deaths")
-#     # ax[1].plot(Seconds,newRecovered,label="New Recovered")
-#     ax[1].legend()
+def plotGraph(Name):
+    """Plots the graph using the inputs and then shows them"""
+    fig, ax=plt.subplots(figsize=(10,5))
+    fig.suptitle("Covid-19 cases")
+    ax.set_title("Covid-19 cases in "+Name)
+    ax.set_xlabel("Dates")
+    ax.set_ylabel("Number of people")
+    ax.plot(Seconds,Confirmed,label="Confirmed")
+    ax.plot(Seconds,Deaths,label="Deaths")
+    ax.plot(Seconds,Recovered,label="Recovered")
+    ###Incase I want to re-add all of the rest of the plots.###
+    # ax[1].plot(Seconds,Active,label="Active")
+    # ax[1].plot(Seconds,newCases,label="New Cases")
+    # ax[1].plot(Seconds,newDeaths,label="New Deaths")
+    # ax[1].plot(Seconds,newRecovered,label="New Recovered")
+    ax.legend()
+    plt.tight_layout()
+    #Need to put each label into their own graphs with the major WHO reigons being shown.##Current##
+    plt.show()
 ###Main code###
-
 
 # This puts the data from day_wise file into the dayVals variable
 dayVals=pd.read_csv("Dataset\Covid-19 Dataset\\full_grouped.csv")
-#These separate the data in the UK and not in the UK.
+#These separate the data in the UK, not in the UK as well as, into individual regions.
 UK=dayVals[(dayVals["Country/Region"]==("United Kingdom"))]
 notUK=dayVals[(dayVals["Country/Region"]!=("United Kingdom"))]
 eastMed=dayVals[(dayVals["WHO Region"]==("Eastern Mediterranean"))]
@@ -86,7 +88,7 @@ with open ("Dataset\Covid-19 Dataset\\full_grouped.csv","r") as d:
     lists=csv.reader(d)
     Timelist.append(list[0])
 #Puts the data from the list into reigionList
-plot=regionList(notUK)
+Confirmed, Deaths, Recovered, Active=regionList(notUK)
 
 
 #Allows to see the time by putting the dates in their own seperate list
@@ -102,34 +104,37 @@ for x in dayVals["Date"]:
 # X = np.random.randn(len(Seconds))
 # fig, ax=plt.subplots()
 # ax.plot(Seconds,X)
+################################################
 #Plots the UK and notUK data on the same figure
-fig, ax=plt.subplots(2,figsize=(10,5))
-fig.suptitle("Covid-19 cases")
-ax[1].set_title("Covid-19 cases in the world")
-ax[1].set_xlabel("Dates")
-ax[1].set_ylabel("Number of people")
-ax[1].plot(plot["Seconds"],plot["Confirmed"],label="Confirmed")
-ax[1].plot(plot["Seconds"],plot["Deaths"],label="Deaths")
-ax[1].plot(plot["Seconds"],plot["Recovered"],label="Recovered")
+# fig, ax=plt.subplots(2,figsize=(10,5))
+# fig.suptitle("Covid-19 cases")
+# ax[1].set_title("Covid-19 cases in the world")
+# ax[1].set_xlabel("Dates")
+# ax[1].set_ylabel("Number of people")
+# ax[1].plot(Seconds,Confirmed,label="Confirmed")
+# ax[1].plot(Seconds,Deaths,label="Deaths")
+# ax[1].plot(Seconds,Recovered,label="Recovered")
 ###Incase I want to re-add all of the rest of the plots.###
 # ax[1].plot(Seconds,Active,label="Active")
 # ax[1].plot(Seconds,newCases,label="New Cases")
 # ax[1].plot(Seconds,newDeaths,label="New Deaths")
 # ax[1].plot(Seconds,newRecovered,label="New Recovered")
-ax[1].legend()
-
-ax[0].set_title("Covid-19 cases in the UK")
-ax[0].set_xlabel("Dates")
-ax[0].set_ylabel("Number of people")
-ax[0].plot(plot["Seconds"],UK["Confirmed"],label="Confirmed")
-ax[0].plot(plot["Seconds"],UK["Deaths"],label="Deaths")
-ax[0].plot(plot["Seconds"],UK["Recovered"],label="Recovered")
+# ax[1].legend()
+##################################
+# ax[0].set_title("Covid-19 cases in the UK")
+# ax[0].set_xlabel("Dates")
+# ax[0].set_ylabel("Number of people")
+# ax[0].plot(plot["Seconds"],UK["Confirmed"],label="Confirmed")
+# ax[0].plot(plot["Seconds"],UK["Deaths"],label="Deaths")
+# ax[0].plot(plot["Seconds"],UK["Recovered"],label="Recovered")
+####################################
 ###Incase I want to re-add all of the rest of the plots.###
 # ax[0].plot(Seconds,UK["Active"],label="Active")
 # ax[0].plot(Seconds,UK["New cases"],label="New Cases")
 # ax[0].plot(Seconds,UK["New deaths"],label="New Deaths")
 # ax[0].plot(Seconds,UK["New recovered"],label="New Recovered")
-ax[0].legend()
-plt.tight_layout()
-#Need to put each label into their own graphs with the major WHO reigons being shown.##Current##
-plt.show()
+# ax[0].legend()
+# plt.tight_layout()
+# #Need to put each label into their own graphs with the major WHO reigons being shown.##Current##
+# plt.show()
+plotGraph("the world")
