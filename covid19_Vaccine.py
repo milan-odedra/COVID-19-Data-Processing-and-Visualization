2de3790534ead60610d602793bd2396435887262
Milan Odedra <m.odedra@2021.ljmu.ac.uk>
2023-04-05 19:36:17 +0100

    import libraries

---
+++ covid19_Vaccine.py
@@ -0,0 +1,9 @@
+# Import Libraries
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+# read data into pandas
+df=pd.readcsv("vaccine_data/country_vaccinations")
