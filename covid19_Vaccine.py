8b9f819d79b55d06a2bd22364dd5123030df1666
Milan Odedra <m.odedra@2021.ljmu.ac.uk>
2023-04-16 21:59:22 +0100

    start sorting the data columns

--- covid19_Vaccine.py
+++ covid19_Vaccine.py
@@ -1,3 +1,5 @@
+# covid19 Vaccine dataset | Covid Year 2022
+
 # Import Libraries
 import numpy as np
 import pandas as pd
@@ -13,3 +15,22 @@ print(df.head())
 
 # prints missing values
 print(df.isnull().sum())
+
+# Fill null values with 0 and drop countries with iso_code = 0
+print(df.fillna(0, inplace = True))
+print(df.drop(df.index[df['iso_code'] == 0], inplace = True))
+
+df.isnull().sum()
+
+# show datatypes of columns
+df.info()
+
+# Change data format
+df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+
+# print the columns names 
+print(df.columns)
+
+# remove uneeded data and keep mostly vaccine relevant data
+df.drop(["people_fully_vaccinated","daily_vaccinations_raw","people_fully_vaccinated_per_hundred",
+         "daily_vaccinations_per_million","people_vaccinated_per_hundred", "source_name","source_website"],axis=1, inplace=True)
