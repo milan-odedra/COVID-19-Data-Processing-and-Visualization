37cb814e65ee9806ba0efe241aa70586a5e5b876
Milan Odedra <m.odedra@2021.ljmu.ac.uk>
2023-04-16 22:35:04 +0100

    start plotting functions of UK, India vaccine data

--- covid19_Vaccine.py
+++ covid19_Vaccine.py
@@ -1,4 +1,4 @@
-# covid19 Vaccine dataset | Covid Year 2022
+# covid19 Vaccine dataset | Covid Year 2021-2022
 
 # Import Libraries
 import numpy as np
@@ -34,3 +34,46 @@ print(df.columns)
 # remove uneeded data and keep mostly vaccine relevant data
 df.drop(["people_fully_vaccinated","daily_vaccinations_raw","people_fully_vaccinated_per_hundred",
          "daily_vaccinations_per_million","people_vaccinated_per_hundred", "source_name","source_website"],axis=1, inplace=True)
+
+# Vaccine Data for India (Bharat)
+
+# Save India data in a first dataframe
+df_India = df[df["iso_code"] == 'IND'].copy()
+print(df_India)
+
+# drop data which is null
+df_India.drop(df_India.index[df_India['total_vaccinations'] == 0], inplace = True)
+
+# Plot the total vacinations as a data function
+plt.figure(figsize=(18,6))
+sns.lineplot(data=df_India, x="date", y="total_vaccinations")
+plt.title("Total number of Vaccinations - India")
+plt.show()
+
+# Plot daily vaccionations as a data function
+plt.figure(figsize=(18,6))
+sns.lineplot(data=df_India, x="date", y="daily_vaccinations")
+plt.title("Daily number of Vaccinations - India")
+plt.xticks(rotation=90)
+plt.show()
+
+
+# ___________________________
+
+# Vaccine data for United Kingdom
+
+# Save United Kingdom data into second dataframe
+df_UK = df[df["iso_code"] == 'UK'].copy()
+df_UK
+
+# Drop the dates with values equal to 0
+df_UK.drop(df_UK.index[df_India['total_vaccinations'] == 0], inplace = True)
+
+# Plot the total number of vaccinations for UK
+plt.figure(figsize=(18,6))
+sns.lineplot(data=df_UK, x="date", y="total_vaccinations")
+plt.title("Total vaccinations in the UK")
+plt.xticks(rotation=45)
+plt.show
+
+# Plot daily vaccinations as a data function
