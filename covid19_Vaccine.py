d420b55ed5713bf3f157c9064fff30a6c58fdbef
Milan Odedra <m.odedra@2021.ljmu.ac.uk>
2023-04-16 21:00:55 +0100

    moved file

--- covid19_Vaccine.py
+++ covid19_Vaccine.py
@@ -6,4 +6,6 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 
 # read data into pandas
-df=pd.readcsv("vaccine_data/country_vaccinations")
+df=pd.read_csv("vaccine_data/country_vaccinations.csv")
+
+df.head()
