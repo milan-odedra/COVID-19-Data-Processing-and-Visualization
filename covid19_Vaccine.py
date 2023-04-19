7532575931179710cd5ca29e92dbbcb564ae7489
Milan Odedra <m.odedra@2021.ljmu.ac.uk>
2023-04-19 17:53:38 +0100

    Cleaned code text adding capitals for readbillity

--- covid19_Vaccine.py
+++ covid19_Vaccine.py
@@ -95,7 +95,7 @@ while (leave==False):
         plt.figure(figsize=(18,6))
         sns.lineplot(data=df_India, x="date", y="daily_vaccinations")
         plt.title("Daily number of Vaccinations - India")
-        plt.xticks(rotation=90)
+        plt.xticks(rotation=45)
         plt.show()
 
     elif Menu=="4":
@@ -112,16 +112,16 @@ while (leave==False):
         plt.figure(figsize=(18,6))
         sns.lineplot(data=df_GBR, x="date", y="daily_vaccinations")
         plt.title("Daily number of Vaccinations - United Kingdom")
-        plt.xticks(rotation=90)
+        plt.xticks(rotation=45)
         plt.show() 
 
     elif Menu=="6":
         # Countries total vaccinations
         plt.figure(figsize=(18,6))
         plt.bar(vaccines_per_country.index, vaccines_per_country.total_vaccinations)
-        plt.title('Total vaccinations per country')
-        plt.xticks(rotation = 90)
-        plt.ylabel('Total vaccinations')
+        plt.title('Total vaccinations Per Country')
+        plt.xticks(rotation = 45)
+        plt.ylabel('Total Vaccinations')
         plt.xlabel('Country')
         plt.show()
 
@@ -129,17 +129,17 @@ while (leave==False):
         # plot bar chart of vaccines per houndred poeple 
         plt.figure(figsize=(18,6))
         plt.bar(vaccines_per_country.index, vaccines_per_country.total_vaccinations_per_hundred)
-        plt.title('Vaccinations per 100 people')
-        plt.xticks(rotation = 90)
-        plt.ylabel('Vaccinations per 100')
+        plt.xticks(rotation = 45)
+        plt.ylabel('Vaccinations per 100 People')
         plt.xlabel('Country')
         plt.show()
 
     elif Menu=="8":
         # plot bar chart to show which vaccines are being taken the most
         plt.figure(figsize=(12,8))
+        plt.title("Most Used Vaccines by Country")
         sns.barplot(data = vaccination_names_by_country, x='vaccines', y = 'total_vaccinations', hue= 'country', dodge=False)
-        plt.xticks(rotation=90)
+        plt.xticks(rotation=45)
         plt.show()
 
     elif Menu=="9":
@@ -147,9 +147,10 @@ while (leave==False):
         fig = px.choropleth(df.reset_index(), locations="iso_code",
                             color="total_vaccinations_per_hundred",
                             color_continuous_scale=px.colors.sequential.Electric,
-                            title= "Total vaccinations per 100")
+                            title= "Total Vaccinations per 100 People")
 
-        fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})  #No margin on left, right, top and bottom
+        # Set plot layout with no margins on left, right, top, and bottom
+        fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})  
         fig.show()  
     
     elif Menu==('Q'):
