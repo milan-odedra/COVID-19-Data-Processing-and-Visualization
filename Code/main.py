a6ea18a03f64be58432481fe537e39567227023f
limescha22 <emil.schallwig@gmail.com>
2023-04-20 12:38:06 +0100

    Removing unnused functions

--- Code/main.py
+++ Code/main.py
@@ -53,7 +53,7 @@ class App(customtkinter.CTk):
 
         # Create sidebar frame with widgets
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
-        self.sidebar_frame.grid(row=0, column=0, rowspan=1, sticky="nsew")
+        self.sidebar_frame.grid(row=0, column=0, rowspan=3, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
 
         self.sidebar_frame_A = customtkinter.CTkButton(self.sidebar_frame,text="Everybody look at this amazing GUI! It deserves 100%")
@@ -108,11 +108,7 @@ class App(customtkinter.CTk):
         self.Textbox.insert("0.0", text=f"This is out data. Feel free to scroll through \n {dframe.to_string()}")
        
 
-        
-
-    def open_input_dialog_event(self):
-        dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
-        print("CTkInputDialog:", dialog.get_input())
+    
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
@@ -120,13 +116,6 @@ class App(customtkinter.CTk):
     def change_scaling_event(self, new_scaling: str):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         customtkinter.set_widget_scaling(new_scaling_float)
-
-    def sidebar_button_event(self):
-        print("sidebar_button click")
-        
-    def button_click(self,button_label):
-        print("Button", button_label, "pressed")
-
     
 
 if __name__ == "__main__":
