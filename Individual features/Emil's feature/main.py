a247e7641269c6ef09105d58112e86ae3885312b
limescha22 <emil.schallwig@gmail.com>
2023-04-20 13:45:38 +0100

    Features of each individual

---
+++ Individual features/Emil's feature/main.py
@@ -0,0 +1,136 @@
+import customtkinter
+from PIL import Image
+import os
+import pandas as pd
+ 
+
+customtkinter.set_appearance_mode("Light")
+customtkinter.set_default_color_theme("blue")
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        
+        # configure window
+        self.title("Homepage for Team 6's Covid-19 Analysis")
+        self.geometry(f"{1100}x{580}")
+
+        # configure grid layout (4x4)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+
+        # Earth image
+        globe = Image.open("Icons/earth.png")
+        globe = globe.resize((25, 25))
+        globeIMG = customtkinter.CTkImage(globe)
+
+        # Graph image
+        graph = Image.open("Icons/wave-graph.png")
+        graph = graph.resize((25, 25))
+        graphIMG = customtkinter.CTkImage(graph)
+
+        # Statistics image
+        statc = Image.open("Icons/statistics.png")
+        statc = statc.resize((25, 25))
+        pstatIMG = customtkinter.CTkImage(statc)
+
+        # Statistics image
+        statp = Image.open("Icons/pie-chart.png")
+        statp = statp.resize((25, 25))
+        cstatIMG = customtkinter.CTkImage(statp)
+
+        # Search image
+        search = Image.open("Icons/magnifier.png")
+        search = search.resize((25, 25))
+        searchIMG = customtkinter.CTkImage(search)
+
+        # Specify the path to the Python file you want to run
+        mapPath = "Code/heatmap.py"
+        WHOpath = "Code/lineGraph.py"
+        menuPath = "Code/DropDownMenu.py"
+        vacpath = "Code/covid19_Vaccine.py"
+
+        # Create sidebar frame with widgets
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=3, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+
+        self.sidebar_frame_A = customtkinter.CTkButton(self.sidebar_frame,text="Everybody look at this amazing GUI! It deserves 100%")
+        self.sidebar_frame_A.grid(row=1, column=0, padx=20, pady=10)
+        self.sidebar_frame_B = customtkinter.CTkButton(self.sidebar_frame, text="Yep, this is a 100% no doubt")
+        self.sidebar_frame_B.grid(row=2, column=0, padx=20, pady=10)
+        self.sidebar_frame_C = customtkinter.CTkButton(self.sidebar_frame, text="I don't really like this")
+        self.sidebar_frame_C.grid(row=3, column=0, padx=20, pady=10)
+
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Team 6 Data Analysis", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
+        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
+                                                                       command=self.change_appearance_mode_event)
+        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
+        self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
+        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
+                                                               command=self.change_scaling_event)
+        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
+
+        # Dataframe for textbox
+        dframe = pd.read_csv('Data/country_wise_latest.csv', sep=",").iloc[:,:3]
+
+        # Create Textbox
+        self.Textbox = customtkinter.CTkTextbox(self, width=250)
+        self.Textbox.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+
+        
+
+        # Create checkbox and switch frame
+        self.function_buttons_grid = customtkinter.CTkFrame(self)
+        self.function_buttons_grid.grid(row=0, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        self.sidebar_label = customtkinter.CTkLabel(self.function_buttons_grid,text="Our functionalities", font=customtkinter.CTkFont(size=15, weight="bold"))
+        self.sidebar_label.grid(row=1, column=0, padx=20, pady=10)
+        self.sidebar_button_A = customtkinter.CTkButton(self.function_buttons_grid,text="Covid World heat-map ",image=globeIMG, compound=customtkinter.RIGHT, command=lambda: os.system(f"python {mapPath}"))
+        self.sidebar_button_A.grid(row=2, column=0, padx=20, pady=10)
+        self.sidebar_button_B = customtkinter.CTkButton(self.function_buttons_grid, text="Covid Graphs",image=graphIMG,compound=customtkinter.RIGHT,command=lambda: os.system(f"python {WHOpath}"))
+        self.sidebar_button_B.grid(row=3, column=0, padx=20, pady=10)
+        self.sidebar_button_C = customtkinter.CTkButton(self.function_buttons_grid, text="Vaccine Statistics ", image=pstatIMG, compound=customtkinter.RIGHT,command=lambda: os.system(f"python {vacpath}"))
+        self.sidebar_button_C.grid(row=4, column=0, padx=20, pady=10)
+        self.sidebar_button_D = customtkinter.CTkButton(self.function_buttons_grid, text="Country Covid Statistics ", image=cstatIMG, compound=customtkinter.RIGHT,command=lambda: self.button_click("C"))
+        self.sidebar_button_D.grid(row=5, column=0, padx=20, pady=10)
+        self.sidebar_button_E = customtkinter.CTkButton(self.function_buttons_grid, text="Search for Country Information ",image=searchIMG, compound=customtkinter.RIGHT,command=lambda: os.system(f"python {menuPath}"))
+        self.sidebar_button_E.grid(row=6, column=0, padx=20, pady=10)
+        
+        # Set default values
+        self.appearance_mode_optionemenu.set("Light")
+        self.scaling_optionemenu.set("100%")
+        self.sidebar_frame_C.configure(state="disabled")
+        self.Textbox.insert("0.0", text=f"This is out data. Feel free to scroll through \n {dframe.to_string()}")
+       
+
+    
+
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def change_scaling_event(self, new_scaling: str):
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        customtkinter.set_widget_scaling(new_scaling_float)
+    
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
+
+
+#Icon and code sources:
+#Earth free icon (no date) Flaticon. Available at: https://www.flaticon.com/free-icon/earth_44386?term=earth&page=1&position=10&origin=search&related_id=44386 (Accessed: April 19, 2023).
+#Magnifier free icon (no date) Flaticon. Available at: https://www.flaticon.com/free-icon/magnifier_64673?term=search&page=1&position=9&origin=tag&related_id=64673 (Accessed: April 19, 2023).
+#Pie Chart free icon (no date) Flaticon. Available at: https://www.flaticon.com/free-icon/pie-chart_2936690?term=statistics&page=1&position=5&origin=search&related_id=2936690 (Accessed: April 19, 2023).
+#Statistics free icon (no date) Flaticon. Available at: https://www.flaticon.com/free-icon/statistics_7626202?term=statistics&page=4&position=87&origin=search&related_id=7626202 (Accessed: April 19, 2023).
+#Wave Graph free icon (no date) Flaticon. Available at: https://www.flaticon.com/free-icon/wave-graph_3647724?term=graph&page=1&position=11&origin=search&related_id=3647724 (Accessed: April 19, 2023).
+#Schimansky, T. (Feb 5 2023) complex_example.py at master · TomSchimansky/CustomTkinter.
+
+
+
